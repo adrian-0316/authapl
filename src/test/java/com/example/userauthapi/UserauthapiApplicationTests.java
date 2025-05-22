7d9972cb117798c36575afdfbd016fbd144789f9
Adrian <adrianzharcov@gmail.com>
2025-05-22 23:49:16 +0300

    project created

---
+++ src/test/java/com/example/userauthapi/UserauthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserauthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
