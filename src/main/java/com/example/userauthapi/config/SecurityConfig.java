3ed0ec00de57613cd543f9a391f57ee503664937
adrian <adrian-03@mail.ru>
2025-05-25 23:11:01 +0300

    Security and custom userdetales are enabled

--- src/main/java/com/example/userauthapi/config/SecurityConfig.java
+++ src/main/java/com/example/userauthapi/config/SecurityConfig.java
@@ -2,21 +2,43 @@ package com.example.userauthapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("myuser")
+                .password(passwordEncoder().encode("mypassword"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // отключаем CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/ping").permitAll() // разрешаем доступ к /ping
-                        .anyRequest().authenticated() // остальные — требуют авторизации
-                );
-
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()) // стандартная форма логина
+                .csrf(csrf -> csrf.disable()); // отключи, если используешь POST-запросы без CSRF-токена
         return http.build();
     }
 }
